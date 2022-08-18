428c0d7965cea091034f7fabf06732a1da0ba514
MaxKingPor <1512347620@qq.com>
2022-08-19 00:02:23 +0800

    API 文档添加

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,46 @@
+#![allow(dead_code)]
+#![doc = include_str!("../README.md")]
+
 use std::{mem::ManuallyDrop, ptr, collections::HashMap, sync::Mutex};
 
-use windows::{Win32::{System::{Com::{self, IDispatch, DISPPARAMS, VARIANT, VARIANT_0, VARIANT_0_0}, Ole::{self, VariantInit}}, Foundation::BSTR}, core::{InParam, PWSTR, HSTRING}};
+use windows::{Win32::{System::{Com::{self, IDispatch, DISPPARAMS, VARIANT, VARIANT_0, VARIANT_0_0}, Ole}, Foundation::BSTR}, core::{InParam, PWSTR, HSTRING}};
 
-// 在windows-rs 中并未搜索到此参数 使用本地定义 来源:
-// https://docs.microsoft.com/en-us/windows/win32/intl/locale-user-default
+/// 在windows-rs 中并未搜索到此参数 使用本地定义 来源:
+/// 
+/// https://docs.microsoft.com/en-us/windows/win32/intl/locale-user-default
 pub const LOCALE_USER_DEFAULT:u32 = 0x0400;
-pub const EMPTY_ARGS: [VARIANT;0] = [];
+
+
+/// dm.dmsoft API 绑定
 pub struct Dmsoft{
+    /// dm.dmsoft 链接实例
     obj: IDispatch,
+    /// Invoke ID 缓存
     catch: Mutex<HashMap<&'static str, i32>>
 }
 
 
+/// 异常枚举
 #[derive(Debug)]
 pub enum Error {
+    /// 调用Windows API 时产生的Error
     WinError(windows::core::Error),
+    /// 从缓存区获取的 Invoke ID 为 `-1`
     IdError
 }
 
+/// API Result
 type Result<T> = core::result::Result<T, Error>;
+
 /// 大漠插件绑定
 #[allow(non_snake_case)]
 impl Dmsoft{
 
+    /// 新建一个 dm.dmsoft API 绑定实例
+    /// # Examples
+    /// ```
+    /// let dm = Dmsoft::new();
+    /// ```
     pub unsafe fn new() -> windows::core::Result<Self>{
         let guid = Com::CLSIDFromProgID(windows::w!("dm.dmsoft"))?;
         let r = Com::CoCreateInstance::<'_, InParam<'_,_>, IDispatch>(&guid, InParam::null(), Com::CLSCTX_ALL)?;
@@ -32,18 +50,54 @@ impl Dmsoft{
         })
     }
 
-    // CString dmsoft::Ver()
+    /// 返回当前插件版本号
+    /// # The function prototype
+    /// ```C++
+    /// CString dmsoft::Ver()
+    /// ```
+    /// # Args
+    /// 
+    /// # Return
+    /// `String` 当前插件的版本描述字符串
+    /// 
+    /// # Examples
+    /// ```
+    /// let dm = Dmsoft::new();
+    /// // 返回版本号
+    /// let ver = dm.Ver().unwrap();
+    /// println!("Ver: {}", ver);
+    /// ```
     #[allow(const_item_mutation)]
     pub unsafe fn Ver(&self) -> Result<String>{
         const NAME:&'static str = "Ver";
-        let result = self.Invoke(NAME, &mut EMPTY_ARGS)?;
+        let result = self.Invoke(NAME, &mut [])?;
         let result = ManuallyDrop::into_inner(result.Anonymous.Anonymous);
         let result = ManuallyDrop::into_inner(result.Anonymous.bstrVal);
         Ok(result.try_into().unwrap())
 
     }
     
-    // long dmsoft::SetPath(const TCHAR * path)
+    /// 设置全局路径,设置了此路径后,所有接口调用中,相关的文件都相对于此路径. 比如图片,字库等.
+    /// # The function prototype
+    /// ```C++
+    /// long dmsoft::SetPath(const TCHAR * path)
+    /// ```
+    /// 
+    /// # Args
+    /// * `path` 字符串: 路径,可以是相对路径,也可以是绝对路径
+    /// 
+    /// ## Return
+    /// `i32`: 0: 失败 1: 成功
+    /// 
+    /// # Examples
+    /// ``` 
+    /// let dm = Dmsoft::new();
+    /// // 以下代码把全局路径设置到了c盘根目录
+    /// let result = dm.SetPath(r"c:\").unwrap();
+    /// // 如下是把全局路径设置到了相对于当前exe所在的路径
+    /// let result = dm.SetPath(r".\MyData").unwrap();
+    /// // 以上，如果exe在c:\test\a.exe 那么，就相当于把路径设置到了c:\test\MyData
+    /// ```
     pub unsafe fn SetPath(&self, path:&str) -> Result<i32>{
         const NAME:&'static str = "SetPath";
         
@@ -56,7 +110,40 @@ impl Dmsoft{
         
     }
 
-    // CString dmsoft::Ocr(long x1,long y1,long x2,long y2,const TCHAR * color,double sim)
+    /// 识别屏幕范围(x1,y1,x2,y2)内符合color_format的字符串,并且相似度为sim,sim取值范围(0.1-1.0),
+    ///
+    /// 这个值越大越精确,越大速度越快,越小速度越慢,请斟酌使用!
+    /// 
+    /// # The function prototype
+    /// ```C++
+    /// CString dmsoft::Ocr(long x1,long y1,long x2,long y2,const TCHAR * color,double sim)
+    /// ```
+    /// # Args
+    /// * `x1:i32`: 区域的左上X坐标
+    /// * `y1:i32`: 区域的左上Y坐标
+    /// * `x2:i32`: 区域的右下X坐标
+    /// * `y2:i32`: 区域的右下Y坐标
+    /// * `color_format:&str`: 颜色格式串. 可以包含换行分隔符,语法是","后加分割字符串. 具体可以查看下面的示例.注意，RGB和HSV,以及灰度格式都支持.
+    /// * `sim:f64`:相似度,取值范围0.1-1.0
+    /// 
+    /// ## Return
+    /// * `String` 返回识别到的字符串
+    /// # Examples
+    /// ```
+    /// let dm = Dmsoft::new();
+    /// // RGB单色识别
+    /// let s = dm.Ocr(0,0,2000,2000,"9f2e3f-000000",1.0).unwrap();
+    /// // RGB单色差色识别
+    /// let s = dm.Ocr(0,0,2000,2000,"9f2e3f-030303",1.0).unwrap();
+    /// // RGB多色识别(最多支持10种,每种颜色用"|"分割)
+    /// let s = dm.Ocr(0,0,2000,2000,"9f2e3f-030303|2d3f2f-000000|3f9e4d-100000",1.0).unwrap();
+    /// //HSV多色识别(最多支持10种,每种颜色用"|"分割)
+    /// let s = dm.Ocr(0,0,2000,2000,"20.30.40-0.0.0|30.40.50-0.0.0",1.0).unwrap();
+    /// //灰度多色识别(最多支持10种,每种颜色用"|"分割)
+    /// let s = dm.Ocr(0,0,2000,2000,"#40-0|#70-10",1.0).unwrap();
+    /// //识别后,每行字符串用指定字符分割 比如用"|"字符分割
+    /// let s = dm.Ocr(0,0,2000,2000,"9f2e3f-000000,|",1.0).unwrap();
+    /// ```
     pub unsafe fn Ocr(&self, x1:i32,y1:i32,x2:i32,y2:i32,color:&str, sim:f64) -> Result<String>{
         const NAME:&'static str = "Ocr";
 
@@ -74,7 +161,49 @@ impl Dmsoft{
         Ok(result.try_into().unwrap())
     }
 
-    // long dmsoft::FindStr(long x1,long y1,long x2,long y2,const TCHAR * str,const TCHAR * color,double sim,long * x,long * y)
+    /// 在屏幕范围(x1,y1,x2,y2)内,查找string(可以是任意个字符串的组合),并返回符合color_format的坐标位置,相似度sim同Ocr接口描述.
+    ///
+    /// (多色,差色查找类似于Ocr接口,不再重述)
+    /// # The function prototype
+    /// ```C++
+    /// long dmsoft::FindStr(long x1,long y1,long x2,long y2,const TCHAR * str,const TCHAR * color,double sim,long * x,long * y)
+    /// ```
+    /// # Args
+    /// * `x1:i32`: 区域的左上X坐标
+    /// * `y1:i32`: 区域的左上Y坐标
+    /// * `x2:i32`: 区域的右下X坐标
+    /// * `y2:i32`: 区域的右下Y坐标
+    /// * `string:&str`: 待查找的字符串,可以是字符串组合，比如"长安|洛阳|大雁塔",中间用"|"来分割字符串
+    /// * `color_format:&str`: 颜色格式串, 可以包含换行分隔符,语法是","后加分割字符串. 具体可以查看下面的示例 .注意，RGB和HSV,以及灰度格式都支持.
+    /// * `sim:f64`: 相似度,取值范围0.1-1.0
+    /// * `x:*mut i32`: 返回X坐标没找到返回-1
+    /// * `y:*mut i32`: 返回Y坐标没找到返回-1
+    /// # Return
+    /// * `i32`: 返回字符串的索引 没找到返回-1, 比如"长安|洛阳",若找到长安，则返回0
+    /// # Examples
+    /// ```
+    /// let dm = Dmsoft::new();
+    /// let (mut x,mut y) = (0,0);
+    /// 
+    /// let dm_ret = dm.FindStr(0,0,2000,2000,"长安","9f2e3f-000000",1.0,&mut x,&mut y).unwrap();
+    /// if x >= 0 and y >= 0{
+    ///     dm.MoveTo(x, y);
+    /// };
+    /// 
+    /// let dm_ret = dm.FindStr(0,0,2000,2000,"长安|洛阳","9f2e3f-000000",1.0,&mut x,&mut y).unwrap();
+    /// if x >= 0 and y >= 0{
+    ///     dm.MoveTo(x, y);
+    /// };
+    /// 
+    /// // 查找时,对多行文本进行换行,换行分隔符是"|". 语法是在","后增加换行字符串.任意字符串都可以.
+    /// let dm_ret = dm.FindStr(0,0,2000,2000,"长安|洛阳","9f2e3f-000000,|",1.0,&mut x,&mut y).unwrap();
+    /// if x >= 0 and y >= 0{
+    ///     dm.MoveTo(x, y);
+    /// };
+    /// ```
+    /// # Note:
+    /// * 此函数的原理是先Ocr识别，然后再查找。所以速度比FindStrFast要慢，尤其是在字库 很大，或者模糊度不为1.0时。\
+    /// * 一般字库字符数量小于100左右，模糊度为1.0时，用FindStr要快一些,否则用FindStrFast.
     pub unsafe fn FindStr(&self, x1:i32, y1:i32, x2:i32,y2:i32, str:&str, color:&str, sim:f64, x:*mut i32, y:*mut i32)-> Result<i32> {
         const NAME:&'static str = "FindStr";
         let mut px = VARIANT::default();
@@ -103,7 +232,24 @@ impl Dmsoft{
     
     }
 
-    // long dmsoft::GetResultCount(const TCHAR * str)
+    /// 对插件部分接口的返回值进行解析,并返回ret中的坐标个数
+    /// # The function prototype
+    /// ```C++
+    /// long dmsoft::GetResultCount(const TCHAR * str)
+    /// ```
+    /// 
+    /// # Args
+    /// * `str:&str` 部分接口的返回串
+    /// # Return
+    /// `i32` 返回ret中的坐标个数
+    /// 
+    /// # Examples
+    /// ``` 
+    /// let dm = Dmsoft::new();
+    /// 
+    /// let s = dm.FindColorEx(0,0,2000,2000,"123456-000000|abcdef-202020",1.0,0).unwrap();
+    /// let count = dm.GetResultCount(s).unwrap();
+    /// ```
     pub unsafe fn GetResultCount(&self, str: &str) -> Result<i32>{
         const NAME:&'static str = "GetResultCount";
         let mut args = [Dmsoft::bstrVal(str)];
@@ -113,7 +259,31 @@ impl Dmsoft{
         Ok(a)
     }
     
-    // long dmsoft::GetResultPos(const TCHAR * str,long index,long * x,long * y) 
+    /// 对插件部分接口的返回值进行解析,并根据指定的第index个坐标,返回具体的值
+    /// # The function prototype
+    /// ```C++
+    /// long dmsoft::GetResultPos(const TCHAR * str,long index,long * x,long * y) 
+    /// ```
+    /// # Args
+    /// `ret:&str`: 部分接口的返回串
+    /// `index:i32`: 第几个坐标
+    /// `x:*mut i32`: 返回X坐标
+    /// `y:*mut i32`: 返回Y坐标
+    /// 
+    /// ## Return
+    /// `i32`: 0: 失败 1: 成功
+    /// 
+    /// # Examples
+    /// ```
+    /// let dm = Dmsoft::new();
+    /// let (mut x,mut y) = (0,0);
+    /// 
+    /// let s = dm.FindColorEx(0,0,2000,2000,"123456-000000|abcdef-202020",1.0,0).unwrap();
+    /// let count = dm.GetResultCount(s)
+    /// for i in 0..count{
+    ///     let dm_ret = dm.GetResultPos(s,i,&mut x,&mut y).unwrap();
+    /// }
+    /// ```
     pub unsafe fn GetResultPos(&self, str:&str, index:i32, x:*mut i32, y:*mut i32) -> Result<i32>{
         const NAME:&'static str = "GetResultPos";
         let mut px = VARIANT::default();
@@ -136,9 +306,12 @@ impl Dmsoft{
         Ok(result.Anonymous.lVal)
     }
 
-    // long dmsoft::StrStr(const TCHAR * s,const TCHAR * str)
-    // 未在API文档中找到此函数说明
-    pub(crate) unsafe fn StrStr(&self, s:&str, str:&str) -> Result<i32>{
+    /// 未在API文档中找到此函数说明
+    /// # The function prototype
+    /// ```C++
+    /// long dmsoft::StrStr(const TCHAR * s,const TCHAR * str)
+    /// ```
+    pub unsafe fn StrStr(&self, s:&str, str:&str) -> Result<i32>{
         const NAME:&'static str = "StrStr";
         let mut args = [
             Dmsoft::bstrVal(str),
@@ -150,9 +323,48 @@ impl Dmsoft{
     }
 
 
+    /// 未在API文档中找到此函数说明
+    /// # The function prototype
+    /// ```C++
+    /// long dmsoft::SendCommand(const TCHAR * cmd)
+    /// ```
+    pub unsafe fn SendCommand(&self, cmd: &str) -> Result<i32>{
+        const NAME:&'static str = "SendCommand";
+        let mut args = [Dmsoft::bstrVal(cmd)];
+        let result = self.Invoke(NAME, &mut args)?;
+        let result = ManuallyDrop::into_inner(result.Anonymous.Anonymous);
+        Ok(result.Anonymous.lVal)
+    }
 
-
-
+    /// 表示使用哪个字库文件进行识别(index范围:0-99)
+    /// 
+    /// 设置之后，永久生效，除非再次设定
+    /// 
+    /// # The function prototype
+    /// ```C++
+    /// long dmsoft::UseDict(long index)
+    /// ```
+    /// 
+    /// # Args
+    /// * `index:i32`: 字库编号(0-99)
+    /// 
+    /// ## Return
+    /// `i32`: 0: 失败 1: 成功
+    /// 
+    /// # Examples
+    /// ```
+    /// let dm = Dmsoft::new();
+    /// let dm_ret = dm.UseDict(1).unwrap();
+    /// ss = dm.Ocr(0,0,2000,2000,"FFFFFF-000000",1.0).unwrap();
+    /// dm_ret = dm.UseDict(0).unwrap();
+    /// ```
+    pub unsafe fn UseDict(&self, index: i32) -> Result<i32>{
+        const NAME:&'static str = "UseDict";
+        let mut args = [Dmsoft::longVar(index)];
+        let result = self.Invoke(NAME, &mut args)?;
+        let result = ManuallyDrop::into_inner(result.Anonymous.Anonymous);
+        Ok(result.Anonymous.lVal)
+    }
 
 
 
